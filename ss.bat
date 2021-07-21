d72541266f981f865853752b25c769804ed9d601
superpooer <teh.epik.dukk@gmail.com>
2021-07-21 21:20:25 +0100

    change port for syncserv

--- ss.bat
+++ ss.bat
@@ -1,1 +1,1 @@
-syncplay-server --motd-file "C:\Users\fukky\syncservmotd.txt" --disable-ready --disable-chat --max-chat-message-length 250 --max-username-length 60
+syncplay-server --motd-file "C:\Users\fukky\syncservmotd.txt" --disable-ready --disable-chat --max-chat-message-length 250 --max-username-length 60 --port 9993
